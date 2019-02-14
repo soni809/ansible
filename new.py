c148f3b1dea16cc08782d7a86368909e4e1dcfae
root <root@ip-172-31-80-84.ec2.internal>
2019-02-14 06:31:26 +0000

    new files add

---
+++ new.py
@@ -0,0 +1,5 @@
+age = input("Enetr your age")
+if age == 17: 
+  print("can see a rated R movie")
+else: 
+  print("can only see rated PG movies")
---
+++ test.py
@@ -0,0 +1,4 @@
+def is_palindrome(word):
+    return None
+    
+print(is_palindrome('Deleveled'))
